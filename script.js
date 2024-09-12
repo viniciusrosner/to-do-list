e5bc1fa788cb6bf7d8a065b02f8e16620d5e8f21
Vinicius Rosner de Vargas <54963576+viniciusrosner@users.noreply.github.com>
2024-09-12 13:53:54 -0300

    CSS changes, new variables created

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container")
-
-
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
 const addTask = () => {
-    if (inputBox.value === "") {
-        alert("Please write something.")
-    }
+  if (inputBox.value === "") {
+    alert("Please write something.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+};
 
-    else {
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value
-        listContainer.appendChild(li)
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7"
-        li.appendChild(span)
-
-    }
-    inputBox.value = ""
-    saveData();
-}
-
-
-listContainer.addEventListener("click", function (e) {
+listContainer.addEventListener(
+  "click",
+  function (e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData()
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData()
-    }
-}, false
-)
+  },
+  false
+);
 
 inputBox.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-        addTask()
-    }
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
 
-})
+const checkAll = document.getElementById("check-all");
+const uncheckAll = document.getElementById("uncheck-all");
+const deleteAllDone = document.getElementById("delete-all-done");
+const clearAll = document.getElementById("clear-all");
 
-
-const clearAll = () => {
-    
-}
-
-
+checkAll.addEventListener("click", function (e) {
+  listContainer.remove(li);
+});
 
 const saveData = () => {
-    localStorage.setItem("data", listContainer.innerHTML)
-}
+  localStorage.setItem("data", listContainer.innerHTML);
+};
 
 const loadData = () => {
-    listContainer.innerHTML = localStorage.getItem("data")
-}
+  listContainer.innerHTML = localStorage.getItem("data");
+};
 
-loadData()
-
+loadData();
